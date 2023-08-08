40c2225965c2e1b4034df432f0da2fc4dc37d8d0
1004-head <woduf0628@naver.com>
2023-08-08 11:16:04 +0900

    add mpu setting code

---
+++ sys/malloc_thread_safe/mpu_prog.h
@@ -0,0 +1,7 @@
+extern unsigned int Image$$ER_ROM$$Base;
+extern unsigned int Image$$ER_ROM$$Limit;
+extern unsigned int Image$$RW_RAM$$Base;
+extern unsigned int Image$$ARM_LIB_STACK$$ZI$$Limit;
+
+int setMPU(void);
+
