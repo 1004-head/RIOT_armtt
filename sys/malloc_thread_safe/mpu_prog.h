83f609e0265f85f46aa83c7fed5d30dd6a2b77fc
1004-head <woduf0628@naver.com>
2023-08-11 14:51:41 +0900

    fix setMPU() error

--- sys/malloc_thread_safe/mpu_prog.h
+++ sys/malloc_thread_safe/mpu_prog.h
@@ -3,5 +3,5 @@ extern unsigned int Image$$ER_ROM$$Limit;
 extern unsigned int Image$$RW_RAM$$Base;
 extern unsigned int Image$$ARM_LIB_STACK$$ZI$$Limit;
 
-int setMPU(void);
+int setMPU(unsigned int region, unsigned int baseAddress, unsigned int limitAddress, unsigned int rw, unsigned int ex);
 
