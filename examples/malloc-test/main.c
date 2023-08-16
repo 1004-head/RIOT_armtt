f88432686f0af2550ca231815b1f24b86783c4bf
1004-head <woduf0628@naver.com>
2023-08-16 17:06:37 +0900

    fix test code

--- examples/malloc-test/main.c
+++ examples/malloc-test/main.c
@@ -29,25 +29,56 @@ int main(void)
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
-    char *data = (char *)malloc(256);
-    char *data1 = (char *)malloc(258);
-    char *data2 = (char *)malloc(260);
+    char *data = (char *)malloc(32);
+    char *data1 = (char *)malloc(64);
+    char *data2 = (char *)malloc(128);
+    char *data3 = (char *)malloc(256);
+    char *data4 = (char *)malloc(20);
+    char *data5 = (char *)malloc(50);
+    char *data6 = (char *)malloc(100);
+    char *data7 = (char *)malloc(200);
     printf("data ptr : %p\n", data);
     printf("data1 ptr : %p\n", data1);
     printf("data2 ptr : %p\n", data2);
-    if(data){
-      snprintf(data, 10, "Hello\n");
-
-      printf("%s\n", data);
-
-      free(data);
-    }
-    char *data3 = (char *)malloc(256);
     printf("data3 ptr : %p\n", data3);
+    printf("data4 ptr : %p\n", data4);
+    printf("data5 ptr : %p\n", data5);
+    printf("data6 ptr : %p\n", data6);
+    printf("data7 ptr : %p\n", data7);
 
+    if(data || data1 || data2 || data3)
+    {
+    snprintf(data, 32, "Hello\n");
+    snprintf(data1, 64, "hello\n");
+    snprintf(data2, 128, "HI\n");
+    snprintf(data3, 256, "hi\n");
+    printf("%s", data);
+    printf("%s", data1);
+    printf("%s", data2);
+    printf("%s", data3);
+    free(data);
     free(data1);
     free(data2);
     free(data3);
+    }
+
+    char *data8 = (char *)malloc(17);
+    char *data9 = (char *)malloc(44);
+    char *data10 = (char *)malloc(90);
+    char *data11 = (char *)malloc(230);
+    printf("data8 ptr : %p\n", data8);
+    printf("data9 ptr : %p\n", data9);
+    printf("data10 ptr : %p\n", data10);
+    printf("data11 ptr : %p\n", data11);
+
+    free(data4);
+    free(data5);
+    free(data6);
+    free(data7);
+    free(data8);
+    free(data9);
+    free(data10);
+    free(data11);
 
     return 0;
 }
