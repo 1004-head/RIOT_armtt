05ca62815d8e77b16ed026fd6b045cd953d8d069
1004-head <woduf0628@naver.com>
2023-09-04 16:25:31 +0900

    delete mpu_prog module code in malloc_wrapper

--- sys/malloc_thread_safe/mpu_prog.h
+++
@@ -1,7 +0,0 @@
-extern unsigned int Image$$ER_ROM$$Base;
-extern unsigned int Image$$ER_ROM$$Limit;
-extern unsigned int Image$$RW_RAM$$Base;
-extern unsigned int Image$$ARM_LIB_STACK$$ZI$$Limit;
-
-int setMPU(unsigned int region, unsigned int baseAddress, unsigned int limitAddress, unsigned int rw, unsigned int ex);
-
